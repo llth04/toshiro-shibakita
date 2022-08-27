3bb0247aa4fd029dd5dde8e019a4538189543fe6
Isabela Fatima <94409366+llth04@users.noreply.github.com>
2022-08-27 14:30:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-<title>Exemplo PHP</title>
+  <title>Exemplo PHP - Wallace Santos</title>
 </head>
 <body>
 
@@ -15,8 +15,8 @@ echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
 $servername = "54.234.153.24";
 $username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$password = "Mestre123";
+$database = "Banco do Wallace";
 
 // Criar conexão
 
@@ -34,7 +34,7 @@ $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Estado, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
 
 
 if ($link->query($query) === TRUE) {
